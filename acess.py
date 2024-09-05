7e4fe15ea075f1404cb0748b637e7e2f0c9cc7df
Alesssandra F R <arfurlanetto@gmail.com>
2024-09-05 14:45:59 -0300

    itera

---
+++ acess.py
@@ -0,0 +1,13 @@
+xuu = dict(An = 45, En = 46, In = 47, On = 48, un = 49 )
+print(f"lista original {xuu} \n")
+
+print("Iteração sem criar vaiável para a chave, que seria - chave=xuu.keys() \n")
+
+for chave in xuu:          #esta chave é o nome da variável que recebe a chave, se não me engano
+          print(chave, "\n")
+
+
+print("Iteração sem criar vaiável para o valor, que seria - chave=xuu.keys() \n")
+
+for variavelValor in xuu.values():          #esta variavelValoe é o nome da variável que recebe o valor, se não me engano
+          print(variavelValor"\n")

6514e82b4cf872cf2a8e194175b7880fb3ab41d0
Alesssandra F R <arfurlanetto@gmail.com>
2024-09-05 14:27:54 -0300

    mod2

--- ADDePOP.py
+++ ADDePOP.py
@@ -1,8 +1,16 @@
 xis = dict(a = 1, b = 0.4, c = "cc", d = "dd")
-print(f"dicionário original {xis}")
+print(f" dicionário original {xis} \n")
 
 xis ["e"] = 0
-print(f"dicionário ADD1 {xis}")
+print(f"dicionário ADD1 {xis} \n")
 
 xis ["b"] = 1.4
-print(f"dicionário MODIFICADO1 {xis}")
+print(f"dicionário MODIFICADO1 {xis}\n")
+
+del xis["c"]
+print(f"deleta item aenas - {xis}\n")
+
+xis.pop("a")
+print(f"pop remove item e retorna valor {xis}\n")
+
+print("lembrando que \ n pula uma linha \n")
